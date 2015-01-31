e5566ff5c0004d902bbcc50a8321a4b7d69cd702
Pieter <pieter@Pieters-MacBook-Pro.local>
2015-01-31 23:23:34 +0100

    first commit

---
+++ rosenblatt.js
@@ -0,0 +1,9 @@
+Network = function(nodes, weights) {
+    this.nodes = nodes;
+    this.weights = weights;
+};
+
+Network.prototype.predict = function(input) {
+};
+
+module.exports.Network = Network;
